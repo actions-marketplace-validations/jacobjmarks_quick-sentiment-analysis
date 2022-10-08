3806888ec59c3b7ce3f277a76278bfa3a3ac85b1
Jacob Marks <jacobjmarks@outlook.com>
2022-10-08 17:34:08 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
 const Sentiment = require('sentiment');
 
-async function run() {
+try {
     const text = core.getInput("text", { required: true });
     const sentiment = new Sentiment();
     const result = sentiment.analyze(text);
     core.setOutput("result", result);
+} catch (error) {
+    core.setFailed(error);
 }
-
-run();
